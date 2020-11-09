e747a977587006f5ddf029bae6ba625ef1e998a4
DmitryKam <k8741460@gmail.com>
2020-11-09 12:10:21 +0300

    Updates

---
+++ precache-manifest.c988befba52aa6a773e0c1d4fb1724f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b029afb93b21bc0c2be4794675ec6b05",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "8592b681d06439eb04a4",
+    "url": "/myPortfolio/static/css/main.d6bc16d0.chunk.css"
+  },
+  {
+    "revision": "88f6d609c7521e376889",
+    "url": "/myPortfolio/static/js/2.4a559693.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/myPortfolio/static/js/2.4a559693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8592b681d06439eb04a4",
+    "url": "/myPortfolio/static/js/main.6ab06dd7.chunk.js"
+  },
+  {
+    "revision": "0f713a185793586653f3",
+    "url": "/myPortfolio/static/js/runtime-main.d351083c.js"
+  }
+]);
