aad789e620549e6fc00aaacf57185cb0788ebd3e
DmitryKam <k8741460@gmail.com>
2020-12-14 14:17:24 +0300

    2 end1

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -5,10 +5,10 @@ function Nav() {
 
     return (
         <div className={s.nav}>
-            <a className={s.items} href="">Главная</a>
-            <a className={s.items} href="">Скиллы</a>
-            <a className={s.items} href="">Проекты</a>
-            <a className={s.items} href="">Контакты</a>
+            <a className={s.items} href="">General</a>
+            <a className={s.items} href="">Skills</a>
+            <a className={s.items} href="">Project</a>
+            <a className={s.items} href="">Contact</a>
         </div>
     )
 }
